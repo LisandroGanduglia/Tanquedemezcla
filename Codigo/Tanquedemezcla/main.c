289aaf9575645c5aab58c1067fdc16a3bc0bbcc5
LisandroGanduglia <lisandroganduglia@gmail.com>
2021-09-10 20:08:49 -0300

    Codigo version 2

--- Codigo/Tanquedemezcla/main.c
+++ Codigo/Tanquedemezcla/main.c
@@ -2,42 +2,7 @@
 #include <string.h>
 #include "mylib.h"
 
-char * getKey(char *key);
-
 int main(int argc, char const *argv[]) {
-    nivel_tanque config;
-    FILE *conf;
-    char cadena[40], *key, *val;
-    char variables[5][20]= {"NivelLeido","NivelA","NivelB","Tiempomotor","TiempoActivom"},i;
-    if ((conf = fopen("confing.conf","rb")) == NULL){
-        printf("No se encontro archivo de configuracion\n");
-        return 1;
-    }
-
-    fgets(cadena,40,conf);
-    do {
-        key = cadena;
-        if((*key) != '#' && strlen(key) >= 0){
-            val = getKey(key);
-            for(i = 0; i<5; i++){
-                if(!strcmp(key,variables[i])){
-                    switch(i){
-                    case 0: config.n = atoi(val);
-                        break;
-                    case 1: config.na_set = atoi(val);
-                        break;
-                    case 2: config.nb_set = atoi(val);
-                        break;
-                    case 3: config.t = atoi(val);
-                        break;
-                    case 4: config.tm_set = atoi(val);
-                        break;
-                        }
-                }
-            }
-        }
-        fgets(cadena,40,conf);
-    }while (!feof(conf));
 
 estado_tanque estado = cargar; // primer estado
 config = f_inicio();
@@ -53,12 +18,4 @@ while (1){
 }
     return 0;
 }
-char * getKey(char *key){
-char i =0;
 
-    while(*(key+1) != ' '){
-        i++;
-    }
-    *(key+i)=0;
-    return key+i+1;
-}
--- Codigo/Tanquedemezcla/mylib.h
+++ Codigo/Tanquedemezcla/mylib.h
@@ -1,6 +1,8 @@
 #ifndef MYLIB_H_INCLUDED
 #define MYLIB_H_INCLUDED
 #include <stdio.h>
+char * getKey(char *key);
+
 
 
 typedef enum {
@@ -11,17 +13,62 @@ typedef enum {
 
 
 typedef struct{
-    int n; //nivel leido
-    int na_set; //nivel liquido A
-    int nb_set; //nivel liquido B
-    int t; //Tiempo del motor activo leído
-    int tm_set; //Tiempo configurado para el motor activo
+    char n; //nivel leido
+    char na_set; //nivel liquido A
+    char nb_set; //nivel liquido B
+    char t; //Tiempo del motor activo leído
+    char tm_set; //Tiempo configurado para el motor activo
 }nivel_tanque;
 
+nivel_tanque config;
 
-nivel_tanque f_inicio(void); // lee el archivo de configuración y carga las variables.
+
 estado_tanque f_cargar(nivel_tanque);
 estado_tanque f_mezclar(nivel_tanque);
 estado_tanque f_descargar(nivel_tanque);
 
+char * getKey(char *key){
+char i =0;
+
+nivel_tanque f_inicio(){  // lee el archivo de configuración y carga las variables.
+    FILE *conf;
+    char cadena[40], *key, *val;
+    char variables[5][20]= {"NivelLeido","NivelA","NivelB","Tiempomotor","TiempoActivom"},i;
+    if ((conf = fopen("confi.conf","rb")) == NULL){
+        printf("No se encontro archivo de configuracion\n");
+        return ;
+    }
+
+    fgets(cadena,40,conf);
+    do {
+        key = cadena;
+        if((*key) != '#' && strlen(key) >= 0){
+            val = getKey(key);
+            for(i = 0; i<5; i++){
+                if(!strcmp(key,variables[i])){
+                    switch(i){
+                    case 0: config.n = atoi(val);
+                        break;
+                    case 1: config.na_set = atoi(val);
+                        break;
+                    case 2: config.nb_set = atoi(val);
+                        break;
+                    case 3: config.t = atoi(val);
+                        break;
+                    case 4: config.tm_set = atoi(val);
+                        break;
+                        }
+                }
+            }
+        }
+        fgets(cadena,40,conf);
+    }while (!feof(conf));
+return config;
+}
+while(*(key+1) != ' '){
+        i++;
+    }
+    *(key+i)=0;
+    return key+i+1;
+    }
 #endif
